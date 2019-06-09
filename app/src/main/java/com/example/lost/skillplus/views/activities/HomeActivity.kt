5617d0e859dc5e833486287d7dff656fea4d1f80
sherif <sherief33392@gmail.com>
2019-06-09 21:14:15 +0200

    Nav Drawer without back to home

--- app/src/main/java/com/example/lost/skillplus/views/activities/HomeActivity.kt
+++ app/src/main/java/com/example/lost/skillplus/views/activities/HomeActivity.kt
@@ -1,24 +1,21 @@
 package com.example.lost.skillplus.views.activities
 
+
 import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.view.MenuItem
-import android.widget.TextView
 import com.example.lost.skillplus.R
 import com.example.lost.skillplus.models.enums.Keys
 import com.example.lost.skillplus.models.managers.FragmentsManager
-import com.example.lost.skillplus.views.fragments.*
 import com.example.lost.skillplus.models.podos.raw.Notification
-import com.example.lost.skillplus.views.fragments.CategoriesFragment
-import com.example.lost.skillplus.views.fragments.FavoritesFragment
-import com.example.lost.skillplus.views.fragments.NotificationsFragment
+import com.example.lost.skillplus.views.fragments.*
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : NavigationDrawerActivity(), CategoriesFragment.OnFragmentInteractionListener, MySkillsFragment.OnFragmentInteractionListener, MyNeedsFragment.OnFragmentInteractionListener, NotificationsFragment.OnFragmentInteractionListener {
-
+class HomeActivity : NavigationDrawerActivity(), MySkillsFragment.OnFragmentInteractionListener, MyNeedsFragment.OnFragmentInteractionListener {
     override fun onFragmentInteraction(uri: Uri) {
-class HomeActivity : NavigationDrawerActivity() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     private fun loadFragment(item: MenuItem) {
         val tag = item.itemId.toString()
@@ -43,7 +40,6 @@ class HomeActivity : NavigationDrawerActivity() {
         }
     }
 
-    private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         loadFragment(item)
         true
@@ -52,8 +48,11 @@ class HomeActivity : NavigationDrawerActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_home)
         super.onCreate(savedInstanceState)
-        FragmentsManager.replaceFragment(supportFragmentManager, CategoriesFragment.newInstance(), R.id.fragment_container, null, false)
-
-        nav_view2.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        if (intent.getSerializableExtra(Keys.NOTIFICATIONS.key) != null) {
+            FragmentsManager.replaceFragment(supportFragmentManager, NotificationsFragment.newInstance(intent.getSerializableExtra(Keys.NOTIFICATIONS.key) as ArrayList<Notification>?), R.id.fragment_container, null, false)
+        } else {
+            FragmentsManager.replaceFragment(supportFragmentManager, CategoriesFragment.newInstance(), R.id.fragment_container, null, false)
+        }
+        bottom_nav.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
