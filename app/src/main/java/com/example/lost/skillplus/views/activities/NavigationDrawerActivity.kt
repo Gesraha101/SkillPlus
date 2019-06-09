8864f4174436a91a06a825a333e50cc80136797b
sherif <sherief33392@gmail.com>
2019-06-09 20:55:28 +0200

    Nav Drawer without back to home

--- app/src/main/java/com/example/lost/skillplus/views/activities/NavigationDrawerActivity.kt
+++ app/src/main/java/com/example/lost/skillplus/views/activities/NavigationDrawerActivity.kt
@@ -5,13 +5,18 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
 import com.bumptech.glide.Glide
 import com.example.lost.skillplus.R
 import com.example.lost.skillplus.models.managers.PreferencesManager
 import com.example.lost.skillplus.models.podos.raw.User
+import com.example.lost.skillplus.views.fragments.MyNeedsFragment
+import com.example.lost.skillplus.views.fragments.MySkillsFragment
 import kotlinx.android.synthetic.main.activity_navigation_drawer.*
-import kotlinx.android.synthetic.main.app_bar_navigation_drawer.*
 import kotlinx.android.synthetic.main.nav_header_navigation_drawer.view.*
 
 
@@ -22,9 +27,11 @@ open class NavigationDrawerActivity : AppCompatActivity(), NavigationView.OnNavi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation_drawer)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         activeUser = PreferencesManager(this@NavigationDrawerActivity).getUser()
         nav_view.getHeaderView(0).user_name.text = activeUser.name
+        nav_view.setNavigationItemSelectedListener(this)
         Glide.with(this)
                 .load(activeUser.pic)
                 .into(nav_view.getHeaderView(0).user_image)
@@ -34,7 +41,6 @@ open class NavigationDrawerActivity : AppCompatActivity(), NavigationView.OnNavi
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
-
     }
 
     override fun onBackPressed() {
@@ -49,20 +55,41 @@ open class NavigationDrawerActivity : AppCompatActivity(), NavigationView.OnNavi
         // Handle navigation view item clicks here.
         when (item.itemId) {
             R.id.nav_skills-> {
-//                    findViewById<FrameLayout>(R.id.framecategory).visibility= View.VISIBLE
-//                    findViewById<FrameLayout>(R.id.frameID).visibility= View.GONE
+
+                findViewById<FrameLayout>(R.id.fragment_container).visibility = View.VISIBLE
+
+                supportFragmentManager.beginTransaction().replace(
+                        R.id.fragment_container,
+                        MySkillsFragment()).commit()
+                nav_view.setCheckedItem(R.id.nav_skills)
+                Log.d("test", "nav_skills")
             }
             R.id.nav_needs-> {
-//                findViewById<FrameLayout>(R.id.framecategory).visibility= View.GONE
-//                findViewById<FrameLayout>(R.id.frameID).visibility= View.VISIBLE
-//                supportFragmentManager.beginTransaction().replace(
-//                        R.id.frameID , MySkillsFragment()
-//                ).commit()
+                findViewById<FrameLayout>(R.id.fragment_container).visibility = View.VISIBLE
+                supportFragmentManager.beginTransaction().replace(
+                        R.id.fragment_container,
+                        MyNeedsFragment()).commit()
+                nav_view.setCheckedItem(R.id.nav_needs)
+                Log.d("test", "nav_needs")
             }
             R.id.nav_active_jobs -> {
+//                findViewById<FrameLayout>(R.id.fragment_container).visibility = View.VISIBLE
+//                supportFragmentManager.beginTransaction().replace(
+//                        R.id.fragment_container,My
+//                        ).commit()
+//                nav_view.setCheckedItem(R.id.nav_needs)
+//                Log.d("test", "nav_needs")
 
             }
             R.id.nav_settings -> {
+                findViewById<FrameLayout>(R.id.fragment_container).visibility = View.GONE
+//                supportFragmentManager.beginTransaction().replace(
+//                        R.id.fragment_container,
+//                        CategoriesFragment()).commit()
+//                nav_view.setCheckedItem(R.id.nav_needs)
+//                Log.d("test", "nav_needs")
+
+
 
             }
             R.id.nav_about_us -> {
