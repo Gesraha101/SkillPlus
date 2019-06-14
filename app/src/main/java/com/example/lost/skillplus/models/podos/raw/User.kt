fba8fe83e43c7c61046a98fd63f195ef6a2c0f96
sherif <sherief33392@gmail.com>
2019-06-14 22:10:49 +0200

    Log out

--- app/src/main/java/com/example/lost/skillplus/models/podos/raw/User.kt
+++ app/src/main/java/com/example/lost/skillplus/models/podos/raw/User.kt
@@ -1,6 +1,6 @@
 package com.example.lost.skillplus.models.podos.raw
 
-data class User(var id: Int = 0,
+data class User(var id: Int? = 0,
                 var name: String? = null,
                 var email: String? = null,
                 var password: String? = null,
