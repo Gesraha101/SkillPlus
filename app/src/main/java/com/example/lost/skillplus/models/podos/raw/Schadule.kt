84ec17e999a4b9a3c235a2a96fd1d8260b49821d
sherif <sherief33392@gmail.com>
2019-06-19 22:37:59 +0200

    payment

--- app/src/main/java/com/example/lost/skillplus/models/podos/raw/Schadule.kt
+++ app/src/main/java/com/example/lost/skillplus/models/podos/raw/Schadule.kt
@@ -1,20 +1,17 @@
 package com.example.lost.skillplus.models.podos.raw
 
-import java.text.SimpleDateFormat
-import java.util.*
-
 class Schadule {
 
     var isSelected: Boolean = false
     var schedulee: String? = null
 
     fun getSchedule(): String {
-        val date = schedulee?.toLong()?.let { Date(it) }
-        val format = SimpleDateFormat("yyyy.MM.dd HH:mm")
-        return format.format(date)
+//        val date = schedulee?.toLong()?.let { Date(it) }
+//        val format = SimpleDateFormat("yyyy.MM.dd HH:mm")
+//        return format.format(date)
 
 //
-//        return this.schedulee.toString()
+        return this.schedulee.toString()
     }
 
     fun setSchedule(x: String) {
